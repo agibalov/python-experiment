78ba59073a7bc4347eba42d4ef027ec5b9456904
agibalov <andrey.agibalov@gmail.com>
2019-10-16 14:16:34 -0400

    Add dataclasses-json enum illustration

--- dataclasses-json-experiment/tests/json_test.py
+++ dataclasses-json-experiment/tests/json_test.py
@@ -1,5 +1,12 @@
-from dataclasses import dataclass
-from dataclasses_json import dataclass_json
+from dataclasses import dataclass, field
+from enum import Enum
+
+from dataclasses_json import dataclass_json, config
+
+
+class Role(Enum):
+    Manager = 1
+    Developer = 2
 
 
 @dataclass_json
@@ -7,25 +14,36 @@ from dataclasses_json import dataclass_json
 class Person:
     name: str
     age: int
+    role: Role = field(metadata=config(
+        encoder=lambda m: m.name,
+        decoder=lambda s: Role[s]))
 
 
 def test_object_to_json():
-    assert Person(name='Andrey', age=100).to_json() == '{"name": "Andrey", "age": 100}'
+    assert Person(
+        name='Andrey',
+        age=100,
+        role=Role.Developer).to_json() == '{"name": "Andrey", "age": 100, "role": "Developer"}'
 
 
 def test_object_from_json():
-    assert Person.from_json('{"name": "John", "age": 123}') == Person(name='John', age=123)
+    assert Person.from_json('{"name": "John", "age": 123, "role": "Developer"}') == Person(
+        name='John',
+        age=123,
+        role=Role.Developer)
 
 
 def test_object_to_dictionary():
-    assert Person.schema().dump(Person(name='John', age=123)) == {
+    assert Person.to_dict(Person(name='John', age=123, role=Role.Developer)) == {
         'name': 'John',
-        'age': 123
+        'age': 123,
+        'role': 'Developer'
     }
 
 
 def test_object_from_dictionary():
-    assert Person.schema().load({
+    assert Person.from_dict({
         'name': 'John',
-        'age': 123
-    }) == Person(name='John', age=123)
+        'age': 123,
+        'role': 'Developer'
+    }) == Person(name='John', age=123, role=Role.Developer)
